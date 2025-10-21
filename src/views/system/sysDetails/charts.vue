ce46896b6e454a3335bb031a07f1d905db6d0245
Lucky-Nicke <glucky.nicke@gmail.com>
2025-10-21 10:09:49 +0800

    Day12-项目结束
    完成了最后的打开日志的板块

--- src/views/system/sysDetails/charts.vue
+++ src/views/system/sysDetails/charts.vue
@@ -1,21 +1,7 @@
 <template>
   <div>
     <el-row>
-      <el-col :span="12">
-        <div id="regesiter_num" :style="{ height: '300px' }"></div
-      ></el-col>
-      <el-col :span="12">
-        <div id="login_num" :style="{ height: '300px' }"></div
-      ></el-col>
-    </el-row>
-
-    <el-row>
-      <el-col :span="12">
-        <div id="video_view_num" :style="{ height: '300px' }"></div
-      ></el-col>
-      <el-col :span="12">
-        <div id="pic_view_num" :style="{ height: '300px' }"></div
-      ></el-col>
+      <el-col> <div id="login_num" :style="{ height: '300px' }"></div></el-col>
     </el-row>
   </div>
 </template>
@@ -25,34 +11,71 @@ export default {
   data() {
     return {};
   },
-  // 用created 有点问题
   mounted() {
-    this.initCharts("regesiter_num", "注册人数统计");
     this.initCharts("login_num", "登录人数统计");
-    this.initCharts("video_view_num", "视频访问人数统计");
-    this.initCharts("pic_view_num", "图片访问人数统计");
   },
   methods: {
     initCharts(type, title) {
-      //   alert(1);
-      // 基于准备好的dom，初始化echarts实例
-      // 基于准备好的dom，初始化echarts实例
-      // 基于准备好的dom，初始化echarts实例
       var myChart = this.$echarts.init(document.getElementById(type));
       myChart.setOption({
         title: {
-          text: title,
+          text: "用户数据统计",
         },
-        tooltip: {},
+        tooltip: {
+          trigger: "axis",
+        },
+        legend: {
+          data: ["Email", "Union Ads"],
+        },
+        grid: {
+          left: "3%",
+          right: "4%",
+          bottom: "15%",
+          containLabel: true,
+        },
+        toolbox: {
+          feature: {
+            saveAsImage: {},
+          },
+        },
         xAxis: {
-          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+          type: "category",
+          boundaryGap: false,
+          data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
         },
-        yAxis: {},
+        yAxis: {
+          type: "value",
+        },
         series: [
           {
-            name: "销量",
+            name: "Email",
             type: "line",
-            data: [5, 20, 36, 10, 10, 20],
+            stack: "Total",
+            data: [120, 132, 101, 134, 90, 230, 210],
+          },
+          {
+            name: "Union Ads",
+            type: "line",
+            stack: "Total",
+            data: [220, 182, 191, 234, 290, 330, 310],
+          },
+          {
+            name: "Video Ads",
+            type: "line",
+            stack: "Total",
+            data: [150, 232, 201, 154, 190, 330, 410],
+          },
+          {
+            name: "Direct",
+            type: "line",
+            stack: "Total",
+            data: [320, 332, 301, 334, 390, 330, 320],
+          },
+          {
+            name: "Search Engine",
+            type: "line",
+            stack: "Total",
+            data: [820, 932, 901, 934, 1290, 1330, 1320],
           },
         ],
       });
